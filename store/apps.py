afc6465ab613d4fa602a363734340897ab94c180
alijahan365 <jahan242-15-846@diu.edu.bd>
2026-08-04 14:08:55 +0600

    Automatically create superuser alijahan on app startup

--- store/apps.py
+++ store/apps.py
@@ -3,3 +3,28 @@ from django.apps import AppConfig
 
 class StoreConfig(AppConfig):
     name = 'store'
+
+    def ready(self):
+        try:
+            import os
+            from django.contrib.auth import get_user_model
+            User = get_user_model()
+            username = os.environ.get('ADMIN_USERNAME', None).strip()
+            password = os.environ.get('ADMIN_PASSWORD', None).strip()
+            email = os.environ.get('ADMIN_EMAIL', 'jahan242-15-846@diu.edu.bd').strip()
+
+            user = User.objects.filter(username=username).first()
+            if not user:
+                User.objects.create_superuser(
+                    username=username,
+                    email=email,
+                    password=password
+                )
+            else:
+                user.set_password(password)
+                user.is_superuser = True
+                user.is_staff = True
+                user.is_active = True
+                user.save()
+        except Exception:
+            pass
